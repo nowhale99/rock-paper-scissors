7d64c7ea08e6ae024e5a5f7c3300594fced3dc01
nowhale99 <119288678+nowhale99@users.noreply.github.com>
2022-12-15 15:31:44 +0300

    Create playRound function

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+
+
 function getComputerChoice() {
     const array = ['rock', 'paper', 'scissors']
     const computerChoice = array[Math.floor(Math.random()*array.length)]
-    console.log(computerChoice)
+    return computerChoice
 }
+
+const playerSelection = prompt('Choose your weapon!').toLowerCase()
+const computerSelection = getComputerChoice()
+
+function playRound(playerSelection, computerSelection) {
+    let result = ''
+
+
+    if (playerSelection === computerSelection) {
+
+        result = 'It\'s a tie game'
+
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors')
+    || (playerSelection === 'scissors' && computerSelection === 'paper')
+    || (playerSelection === 'paper' && computerSelection === 'rock')) {
+
+        result = `You win! ${playerSelection} beats ${computerSelection}!`
+
+    } else {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}!`
+    }
+
+    return result
+
+}
+
+console.log(playRound(playerSelection, computerSelection))
